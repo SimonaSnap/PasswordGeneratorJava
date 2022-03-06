374b5695a7589bec031aa57f807fe92e847dfc81
Simona <simona.snapk@gmail.com>
2022-03-06 00:23:18 -0800

    tried figuring out the concatation loop struggle, have yet to succeed

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,39 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+//var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function writePassword() {
+//  var password = generatePassword();
+//  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//  passwordText.value = password;
 
-}
+//}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
+
+
+//how do i set the passwordLength? 8-128
+//how do i set the need for lowercase?
+//how do i set the need for uppercase?
+//how do i set the need for numbers?
+//how do i set the need for special characters?
+//how do i make sure at least one of each group is selected?
+
+
+var passwordLength = 5;
+var pattern_list = "0123456789"
+var password = ''
+
+function makePassword(passwordLength)
+{
+  password = " "
+  for (i = 0; i < passwordLength; i++)
+  {
+    password += pattern_list.charAt(Math.floor(Math.random() * pattern_list.length))
+    return password;
+  }
+}
+
+makePassword();
