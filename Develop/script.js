63b0cfa48bcefb117e4d487517fcf2e0b32c3f75
Simona <simona.snapk@gmail.com>
2022-03-11 23:32:34 -0800

    the password generator is complete and linked to the button

--- Develop/script.js
+++ Develop/script.js
@@ -2,105 +2,116 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword()
+function writePassword(text)
 {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = text;
 
 }
 
 //2. need to make arrays(buckets) for each category (this goes before function generatePassword())
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var specialChar = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", "\\", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "|", "_", "^", "`", "{", "}", "~"]
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numbers = "0123456789"
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var specialChar = " !\"#$%&'()*+\\,-./:;<>=?@[]|_^`{}~"
+var uppercase = lowercase.toUpperCase()
+
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", startButton);
 
-//getting the user's preferences
-var askPassLength = prompt("How long do you want the password to be? ")
-
-
-var addLowercase = confirm("Would you like to include lowercase characters? ")
-var addUppercase = confirm("Would you like to include uppercase characters? ")
-var addSpecialChar = confirm("Would you like to include special characters? ")
-var addNumbers = confirm("Would you like to include numbers? ")
-
-//how do i consolidate the user's preferences?
-var passLength = 10
-
-var userPreference = []
-
-var finalPassword = []
-
-if (addLowercase)
+//this is the function that connects it to the generateBtn line above
+//it links all the things below to make sure the whole process is linked to the button and therefore is done as many times as the user wants
+function startButton()
 {
-  userPreference += lowercase
-  finalPassword += getRandomLowercase()
-}
+  var askPassLength = prompt("How long do you want the password to be? ", "Please enter a number between 8 and 128")
 
-if (addUppercase)
-{
-  userPreference += uppercase
-  finalPassword += getRandomUppercase()
-}
-
-if (addSpecialChar)
-{
-  userPreference += specialChar
-  finalPassword += getRandomChar()
-}
-
-if (addNumbers)
-{
-  userPreference += numbers
-  finalPassword += getRandomNumber()
-}
-
-console.log(userPreference)
-
-//how do i get a random variable from each array?
-function getRandomNumber()
-{
-  var randNumIndex = Math.floor(numbers.length * Math.random())
-  var randNumValue = numbers[randNumIndex]
-  return randNumValue
-}
-
-function getRandomLowercase()
-{
-  var randLowerIndex = Math.floor(lowercase.length * Math.random())
-  var randLowerValue = lowercase[randLowerIndex]
-  return randLowerValue
-}
-
-function getRandomUppercase()
-{
-  var randUpperIndex = Math.floor(uppercase.length * Math.random())
-  var randUpperValue = uppercase[randUpperIndex]
-  return randUpperValue
-}
-
-function getRandomChar()
-{
-  var randCharIndex = Math.floor(specialChar.length * Math.random())
-  var randCharValue = specialChar[randCharIndex]
-  return randCharValue
-}
-
-//how do i consolidate those into one array
-function makePassword(passLength)
-{
-  var index = 0;
-  for (i = finalPassword.length; i < passLength; i++)
+  //setting the min and max possibilities for the function
+  while (askPassLength < 8 || askPassLength > 128)
   {
-    index = Math.floor(userPreference.length * Math.random())
-    finalPassword += userPreference[index]
+    askPassLength = prompt("How long do you want the password to be? ", "Please enter a number between 8 and 128")
   }
-  return finalPassword
-}
-console.log(makePassword(10))
 
+  //setting the rest of the prompt categories
+  var addLowercase = confirm("Would you like to include lowercase characters? ")
+  var addUppercase = confirm("Would you like to include uppercase characters? ")
+  var addSpecialChar = confirm("Would you like to include special characters? ")
+  var addNumbers = confirm("Would you like to include numbers? ")
+
+  //created a place to consolidate user preferences and also for the final password
+  var userPreference = ""
+  var finalPassword = ""
+
+  //adds the preference to the user preference array
+  // the second += line states that the first part of the array will immediately add one random of each preference
+  // this ensures that the generated password will have at least one of each category
+  if (addLowercase)
+  {
+    userPreference += lowercase
+    finalPassword += getRandomLowercase()
+  }
+
+  if (addUppercase)
+  {
+    userPreference += uppercase
+    finalPassword += getRandomUppercase()
+  }
+
+  if (addSpecialChar)
+  {
+    userPreference += specialChar
+    finalPassword += getRandomChar()
+  }
+
+  if (addNumbers)
+  {
+    userPreference += numbers
+    finalPassword += getRandomNumber()
+  }
+
+  //this sets up the random generator for each category
+  function getRandomNumber()
+  {
+    var randNumIndex = Math.floor(numbers.length * Math.random())
+    var randNumValue = numbers[randNumIndex]
+    return randNumValue
+  }
+
+  function getRandomLowercase()
+  {
+    var randLowerIndex = Math.floor(lowercase.length * Math.random())
+    var randLowerValue = lowercase[randLowerIndex]
+    return randLowerValue
+  }
+
+  function getRandomUppercase()
+  {
+    var randUpperIndex = Math.floor(uppercase.length * Math.random())
+    var randUpperValue = uppercase[randUpperIndex]
+    return randUpperValue
+  }
+
+  function getRandomChar()
+  {
+    var randCharIndex = Math.floor(specialChar.length * Math.random())
+    var randCharValue = specialChar[randCharIndex]
+    return randCharValue
+  }
+
+  //this makes the final password
+  // i starts at finalPassword.length to ensure that it matches the user's desired password length
+  //the var result sets the parameter to askPassLength, so that we know the parameter is what the user's input ends up being
+  function makePassword(passLength)
+  {
+    var index = 0;
+    for (i = finalPassword.length; i < passLength; i++)
+    {
+      index = Math.floor(userPreference.length * Math.random())
+      finalPassword += userPreference[index]
+    }
+    return finalPassword
+  }
+  var result = makePassword(askPassLength)
+  console.log(result)
+  writePassword(result)
+}
